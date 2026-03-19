195cb48903b17111951800a5beb58c1af32a8413
Phoenix Himself <pkaras.it@gmail.com>
2026-03-19 10:26:19 +0100

    Remove stray test script fixtures

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Written in [Amber](https://amber-lang.com/)
-# version: b594975
-white_0="\\033[47m"
-red_1="\\033[41m"
-reset_2="\\033[0m"
-echo "\$${white_0} white text \$${reset_2}"
-echo "\$${red_1} red text \$${reset_2}"
